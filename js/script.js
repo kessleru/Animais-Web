671b0fffe3a129dae0bd8a522c7f8a20959d11d8
Otávio Kessler Ustra <otaviokesslerustra@hotmail.com>
2025-10-17 22:11:49 -0300

    fix: correct import path for tab navigation module

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 import initScrollSuave from "./modules/scroll-suave.js";
 import initAnimacaoScroll from "./modules/scroll-animacao.js";
 import initAccordion from "./modules/accordion.js";
-import initTabNav from "./modules/tabNav.js";
+import initTabNav from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
 
 initScrollSuave();
