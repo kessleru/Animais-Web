e3b0e502ea8ae3523ca42e90106cb2012c5c4c8a
Otávio Kessler Ustra <90390195+kessleru@users.noreply.github.com>
2025-11-21 15:15:27 -0300

    chore: corrigir nome do módulo funcionamento

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
-import initFuncionamento from './modules/funciomento.js';
+import initFuncionamento from './modules/funcionamento.js';
 import initFetchAnimais from './modules/fetch-animais.js';
 import initFetchBitcoin from './modules/fetch-bitcoin.js';
 
